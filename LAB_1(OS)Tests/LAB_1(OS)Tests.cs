bc34535fe69b5ed9dc2aa40311c7f9b56f8448a1
YourUsa <64005397+YourUsa@users.noreply.github.com>
2021-02-19 17:30:16 +0300

    Изменения в пространстве имен

--- LAB_1(OS)Tests/LAB_1(OS)Tests.cs
+++ LAB_1(OS)Tests/LAB_1(OS)Tests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using LAB_1_OS_;
 
 namespace LAB_1_OS_Tests
 {
@@ -11,7 +12,7 @@ namespace LAB_1_OS_Tests
         {
             int[][] myArray = new int[][] { new int[] {2,3 }, new int[] { 4, 7 }, new int[] { 1, 3 },
             new int[] { 21, 78 }, new int[] { 6, 39 } };
-            int result = SumMaxParallel(myArray, 20);
+            int result = Program.SumMaxParallel(myArray, 20);
             int expected = 130;
             Assert.AreEqual(expected, result);
         }
@@ -20,7 +21,7 @@ namespace LAB_1_OS_Tests
         {
             int[][] myArray = new int[][] { new int[] {2,3 }, new int[] { 4, 7 }, new int[] { 1, 3 },
             new int[] { 21, 78 }, new int[] { 6, 39 }, new int[] { 7, 3 } };
-            int result = SumMaxParallel(myArray, 7);
+            int result = Program.SumMaxParallel(myArray, 7);
             int expected = 137;
             Assert.AreEqual(expected, result);
         }
